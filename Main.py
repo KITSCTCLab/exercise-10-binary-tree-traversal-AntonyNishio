2514aef5716f4d33bb3a80bdc3edcb45cf265dec
AntonyNishio <110011339+AntonyNishio@users.noreply.github.com>
2022-10-11 12:18:53 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ class BinaryTreeNode:
     """
     def __init__(self, data):
         self.data = data
-        self.left_cild = None
+        self.left_child = None
         self.right_child = None
 
 
